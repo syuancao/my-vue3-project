4742e8c32454887a52efa7e7f036e8ad85b814cb
syuancao <1397213571@qq.com>
2021-02-22 19:11:44 +0800

    Adding Events To the Items Component

--- src/components/items/itemsList.component.vue
+++ src/components/items/itemsList.component.vue
@@ -2,7 +2,7 @@
   <div>
     <h3>My Items:</h3>
     <ul>
-      <li v-for="item in items" :key="item.id">
+      <li v-for="item in items" :key="item.id" @click="onItemSelect(item)">
         {{ item.name }}
       </li>
     </ul>
@@ -11,13 +11,22 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
-import { ItemInterface } from '@/models/items/Item.interface';
+import { ItemInterface } from "@/models/items/Item.interface";
 
 export default defineComponent({
   props: {
     items: {
       type: Array as PropType<ItemInterface[]>,
     },
+  },
+  setup() {
+    const onItemSelect = (item: ItemInterface) => {
+      item.selected = !item.selected;
+      console.log("onItemSelect", item.id, item.selected);
+    };
+    return {
+      onItemSelect,
+    };
   },
 });
 </script>
