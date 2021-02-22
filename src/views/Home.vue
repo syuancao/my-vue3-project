5b13a8de398ece3967e12ec0abeae0b554698d7f
syuancao <1397213571@qq.com>
2021-02-22 18:58:37 +0800

    Data Model Interfaces

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,24 +7,29 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import ItemsListComponent from "@/components/items/itemsList.component.vue";
+import { ItemInterface } from "@/models/items/Item.interface";
+
 export default defineComponent({
   name: "Home",
   components: {
     ItemsListComponent,
   },
   setup() {
-    const items: any[] = [
+    const items: ItemInterface[] = [
       {
         id: 1,
         name: "Item 1",
+        selected: false
       },
       {
         id: 2,
         name: "Item 2",
+        selected: false
       },
       {
         id: 3,
         name: "Item 3",
+        selected: false
       },
     ];
     return {
